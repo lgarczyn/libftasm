59ea12a0ec2fdea346deaecaded057d97367adcb
louis <louis.roc@gmail.com>
2019-01-06 10:08:49 +0100

    made strlen use rep because subject

--- main.c
+++ main.c
@@ -20,6 +20,138 @@
 #define			CALL(F) {printf(#F); TRY(ft_##F);}
 #define			TEST(F) {printf(#F); TRY(F);}
 
+void			test_perf(char *n)
+{
+	char		buffer[10000];
+	char		buffer2[10000];
+
+	int fd = open(n, O_RDONLY);
+	int len = read(fd, buffer, 10000);
+	printf("last char: %c\n", buffer[len - 1]);
+
+	long a, b, a_branch, b_branch, a_compact, b_compact;
+
+	a = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint(buffer[i]);
+	b = clock();
+
+	a_branch = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_branch(buffer[i]);
+	b_branch = clock();
+
+	a_compact = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_compact(buffer[i]);
+	b_compact = clock();
+
+	printf("1 cond %lu, 2 branch %lu 3 compact %lu\n", b - a, b_branch - a_branch, b_compact - a_compact);
+
+	a = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint(buffer[i]);
+	b = clock();
+
+	a_compact = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_compact(buffer[i]);
+	b_compact = clock();
+
+	a_branch = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_branch(buffer[i]);
+	b_branch = clock();
+
+	printf("1 cond %lu, 3 branch %lu 2 compact %lu\n", b - a, b_branch - a_branch, b_compact - a_compact);
+
+	a_branch = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_branch(buffer[i]);
+	b_branch = clock();
+
+	a = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint(buffer[i]);
+	b = clock();
+
+	a_compact = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_compact(buffer[i]);
+	b_compact = clock();
+
+	printf("2 cond %lu, 1 branch %lu 3 compact %lu\n", b - a, b_branch - a_branch, b_compact - a_compact);
+
+	a_compact = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_compact(buffer[i]);
+	b_compact = clock();
+
+	a = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint(buffer[i]);
+	b = clock();
+
+	a_branch = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_branch(buffer[i]);
+	b_branch = clock();
+
+	printf("2 cond %lu, 3 branch %lu 1 compact %lu\n", b - a, b_branch - a_branch, b_compact - a_compact);
+
+	a_branch = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_branch(buffer[i]);
+	b_branch = clock();
+
+	a_compact = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_compact(buffer[i]);
+	b_compact = clock();
+
+	a = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint(buffer[i]);
+	b = clock();
+
+	printf("3 cond %lu, 1 branch %lu 2 compact %lu\n", b - a, b_branch - a_branch, b_compact - a_compact);
+
+	a_compact = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_compact(buffer[i]);
+	b_compact = clock();
+
+	a_branch = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint_branch(buffer[i]);
+	b_branch = clock();
+
+	a = clock();
+	for (int j = 0; j < 10000; j++)
+		for (int i = 0; i < len; i++)
+			buffer2[i] = ft_isprint(buffer[i]);
+	b = clock();
+
+	printf("3 cond %lu, 2 branch %lu 1 compact %lu\n", b - a, b_branch - a_branch, b_compact - a_compact);
+}
+
 int				main(int argc, char **argv)
 {
 	char		test[101];
@@ -32,13 +164,18 @@ int				main(int argc, char **argv)
 	CALL(bzero(test, 100));
 	TEST(memcmp(test, test_ref, 100) == 0);
 
+	printf("a.out\n");
+	test_perf("a.out");
+	printf("main.c\n");
+	test_perf("main.c");
+	return (1);
 	for (int i = 0; i < 130; i++)
 	{
-		// if (i == 129)
-		// {
-		// 	i = 10000;
-		// }
-		printf("%i [%c]: %c ~%c O%c 1%c I%c a%c A%c ^%c v%c\n",
+		if (i == 129)
+		{
+			i = 10000;
+		}
+		printf("%i [%c]: %c ~%c O:%c 1:%c I:%c a:%c A:%c U:%c l:%c\n",
 			i,
 			ft_isprint(i) ? i : '?',
 			ft_isascii(i) ? 'Y' : 'N',
@@ -50,6 +187,13 @@ int				main(int argc, char **argv)
 			ft_isupper(i) ? 'Y' : 'N',
 			ft_isprint(i) ? ft_toupper(i) : '?',
 			ft_isprint(i) ? ft_tolower(i) : '?');
+		/* 
+		printf("%i [%c]: %c %c %c\n",
+		i, 
+		ft_isprint(i) ? i : '?',
+		ft_isprint(i) ? 'Y' : 'N',
+		ft_isprint_branch(i) ? 'Y' : 'N',
+		ft_isprint_compact(i) ? 'Y' : 'N'); */
 	}
 
 	CALL(puts("test"));
@@ -66,7 +210,14 @@ int				main(int argc, char **argv)
 	CALL(memcpy(test, test + 50, 50));
 	memcpy(test_ref, test_ref + 50, 50);
 	TEST(memcmp(test, test_ref, 100) == 0);	
+
+	char *a = "";
+	char *b = "fajl;aef";
+	char *c = "fajl;aefdawwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww";
+	printf("%lu %lu\n", strlen(a), ft_strlen(a));
+	printf("%lu %lu\n", strlen(b), ft_strlen(b));
+	printf("%lu %lu\n", strlen(c), ft_strlen(c));
 	
 	CALL(strdup(test));
-	CALL(cat(open("src/ft_strlen.s", O_RDONLY)));
+	//CALL(cat(open("src/ft_strlen.s", O_RDONLY)));
 }
--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -1,12 +1,25 @@
 %include "src/macros.s"
 global ft_strlen
-ft_strlen:
-	mov RAX, RDI									; save RDI to return value
+
+;original
+ft_strlen_fast:
+	cmp [RDI], byte 0		; compare first byte to 0
+	mov RAX, RDI			; save str to return value
+	jz .end					; if first byte was enpty, skip
 .loop:
-	cmp [RAX], byte 0								; check current byte
-	je .end											; if 0, stop
-	inc RAX											; otherwise inc ptr
-	jmp .loop										; go back to start
+	inc RAX					; inc ptr
+	cmp [RAX], byte 0		; check current byte
+	jne .loop				; go back to start if byte is not zero
 .end:
-	sub RAX, RDI									; set RAX as diff between incremented and original pointer
+	sub RAX, RDI			; set RAX as diff between incremented and original str
+	ret
+
+ft_strlen:
+	xor RCX, RCX			; reset RCX
+	not RCX					; set RCX to 0xFFFFFFFF_FFFFFFFF
+	cld						; set direction of rep operation
+	repnz scasb				; while (*RDI) {RDI++, RCX--}
+	not RCX					; recover strlen + 1 from counter
+	dec RCX					; dec to strlen
+	mov RAX, RCX			; move to return value
 	ret
